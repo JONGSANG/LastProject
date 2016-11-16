fd908c820bb1e6551fe3574b6b1b835573886de0
ksh16 <pper5238@gmail.com>
2016-11-16 11:00:32 +0900

    다솜 병합

--- src/main/java/com/kosta/controller/AdminInfoController.java
+++ src/main/java/com/kosta/controller/AdminInfoController.java
@@ -2,14 +2,15 @@ package com.kosta.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminInfoController {
 	
-	@RequestParam(value="sadf", method=RequestMethod.GET)
+	@RequestMapping(value="/adminLibrary/", method=RequestMethod.GET)
 	public String userList(Model model) throws Exception {
+		
 		return "";
 	}
 }
