a0b06d34d54fe8ec5378c6fd7eed667d3487a9dc
nakjun <choirj91@gmail.com>
2016-11-17 20:58:48 +0900

    메인 게시판

--- src/main/java/com/kosta/dao/MainDAO.java
+++
@@ -1,8 +0,0 @@
-package com.kosta.dao;
-
-import com.kosta.vo.MemberVO;
-
-public interface MainDAO {
-
-	public MemberVO getName(String id) throws Exception;
-}
--- src/main/java/com/kosta/service/MainService.java
+++
@@ -1,7 +0,0 @@
-package com.kosta.service;
-
-import com.kosta.vo.MemberVO;
-
-public interface MainService {
-	public MemberVO getName() throws Exception;
-}
