52014d1a51faf26a7f390b08a58371862cdb3798
A\A <vlkm92@naver.com>
2016-11-28 14:22:01 +0900

    날짜VO들 String 에서 Date타입으로 변경 / 공개게시판 list.jsp 날짜 fmt로 수정

--- src/main/java/com/kosta/vo/L_CultureVO.java
+++ src/main/java/com/kosta/vo/L_CultureVO.java
@@ -1,9 +1,12 @@
 package com.kosta.vo;
 
+import java.util.Date;
+
 public class L_CultureVO {
 	
 	private int num, anum, state;
-	private String id, culture_date;
+	private String id;
+	private Date  culture_date;
 	
 	public int getNum() {
 		return num;
@@ -29,10 +32,10 @@ public class L_CultureVO {
 	public void setId(String id) {
 		this.id = id;
 	}
-	public String getCulture_date() {
+	public Date getCulture_date() {
 		return culture_date;
 	}
-	public void setCulture_date(String culture_date) {
+	public void setCulture_date(Date culture_date) {
 		this.culture_date = culture_date;
 	}
 }
