3bcd31c0bd650bcf4dc08aac1f62c31d245b7004
ksh16 <pper5238@gmail.com>
2016-11-28 18:00:02 +0900

    임시

--- src/main/java/com/kosta/dao/AdminUserDAOImpl.java
+++ src/main/java/com/kosta/dao/AdminUserDAOImpl.java
@@ -1,15 +1,19 @@
 package com.kosta.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminUserDAOImpl extends AdminUserDAO {
+public class AdminUserDAOImpl implements AdminUserDAO {
+
 	
-	
+	@Autowired
+	private SqlSession sqlSession;
 	
 	@Override
-	default String[] allMember() throws Exception {
+	public String[] allMember() throws Exception {
+		// TODO Auto-generated method stub
 		return null;
 	}
-	
 }
