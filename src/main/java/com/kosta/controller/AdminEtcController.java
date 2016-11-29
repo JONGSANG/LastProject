ff31d5c21828d3abd3d198112f7dab40ed2ca8a2
ksh16 <pper5238@gmail.com>
2016-11-29 21:36:18 +0900

    하기 귀찮아

--- src/main/java/com/kosta/controller/AdminEtcController.java
+++ src/main/java/com/kosta/controller/AdminEtcController.java
@@ -3,11 +3,19 @@ package com.kosta.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminEtcController {
 
 		private static final Logger logger = LoggerFactory.getLogger(AdminEtcController.class);
 		
-		
+		@RequestMapping(value="adminLibrary/adminEtc/mBoard")
+		public String mBoardGET() throws Exception {
+			
+			logger.info("문화행사 리스트");
+			
+			
+			return "adminLibrary/adminEtc/mBoard";
+		}
 }
